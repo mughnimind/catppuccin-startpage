e2b733d60436ec4cd9a508b0aae6e2e240a547ce
mughnimind <ac.mughni@gmail.com>
2025-10-09 14:06:39 +0700

    ganti perplex ke startpage

--- userconfig.js
+++ userconfig.js
@@ -39,7 +39,7 @@ const default_configuration = {
   disabled: [],
   localIcons: true,
   localFonts: true,
-  fastlink: "https://www.perplexity.ai",
+  fastlink: "vivaldi://startpage",
   openLastVisitedTab: true,
   tabs: [
     {
